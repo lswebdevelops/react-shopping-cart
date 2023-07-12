fe00c2baae9e87da52213a748dedad45a095e06c
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-12 15:52:44 -0300

    adding websites to data

--- src/assets/reitsData.js
+++ src/assets/reitsData.js
@@ -6,89 +6,99 @@ const reitsData = [
     price2023: 200.5,
     size: 90,
     type: "reits",
-    description: "American Tower Corporation is a real estate investment trust (REIT) that owns and operates wireless communication infrastructure, including cell towers and antenna systems. The company leases space on its towers to wireless service providers, enabling reliable and efficient wireless communication. With a vast portfolio of tower assets globally, American Tower plays a critical role in supporting the expanding mobile connectivity needs of the digital age."
-    },
-    {
+    description: "American Tower Corporation is a real estate investment trust (REIT) that owns and operates wireless communication infrastructure, including cell towers and antenna systems. The company leases space on its towers to wireless service providers, enabling reliable and efficient wireless communication. With a vast portfolio of tower assets globally, American Tower plays a critical role in supporting the expanding mobile connectivity needs of the digital age.",
+    website: "https://www.americantower.com/"
+  },
+  {
     id: 1001,
     ticker: "PLD",
     name: "Prologis, Inc.",
     price2023: 250.2,
     size: 110,
     type: "reits",
-    description: "Prologis, Inc. is a leading logistics real estate investment trust (REIT) that owns, operates, and develops industrial properties worldwide. The company's properties serve as distribution centers and warehouses for various industries, including e-commerce, retail, and manufacturing. Prologis plays a vital role in facilitating supply chain operations and supporting the growth of e-commerce and global trade."
-    },
-    {
+    description: "Prologis, Inc. is a leading logistics real estate investment trust (REIT) that owns, operates, and develops industrial properties worldwide. The company's properties serve as distribution centers and warehouses for various industries, including e-commerce, retail, and manufacturing. Prologis plays a vital role in facilitating supply chain operations and supporting the growth of e-commerce and global trade.",
+    website: "https://www.prologis.com/"
+  },
+  {
     id: 1002,
     ticker: "EQIX",
     name: "Equinix, Inc.",
     price2023: 300.7,
     size: 70,
     type: "reits",
-    description: "Equinix, Inc. is a global data center real estate investment trust (REIT) that provides colocation, interconnection, and cloud services. The company operates a network of highly secure and interconnected data centers, enabling businesses to connect and exchange data efficiently. Equinix's data centers serve as critical infrastructure for cloud computing, digital content delivery, and network connectivity."
-    },
-    {
+    description: "Equinix, Inc. is a global data center real estate investment trust (REIT) that provides colocation, interconnection, and cloud services. The company operates a network of highly secure and interconnected data centers, enabling businesses to connect and exchange data efficiently. Equinix's data centers serve as critical infrastructure for cloud computing, digital content delivery, and network connectivity.",
+    website: "https://www.equinix.com/"
+  },
+  {
     id: 1003,
     ticker: "PSA",
     name: "Public Storage",
     price2023: 180.4,
     size: 80,
     type: "reits",
-    description: "Public Storage is a real estate investment trust (REIT) that specializes in self-storage facilities. The company owns and operates a large portfolio of storage properties, providing individuals and businesses with secure and convenient storage solutions. With a wide geographic presence, Public Storage serves the growing demand for storage space in various markets."
-    },
-    {
+    description: "Public Storage is a real estate investment trust (REIT) that specializes in self-storage facilities. The company owns and operates a large portfolio of storage properties, providing individuals and businesses with secure and convenient storage solutions. With a wide geographic presence, Public Storage serves the growing demand for storage space in various markets.",
+    website: "https://www.publicstorage.com/"
+  },
+  {
     id: 1004,
     ticker: "SPG",
     name: "Simon Property Group",
     price2023: 150.8,
     size: 120,
     type: "reits",
-    description: "Simon Property Group is a real estate investment trust (REIT) that focuses on owning and operating shopping malls, premium outlets, and other retail properties. The company's properties provide a diverse range of retail experiences, attracting shoppers and tenants alike. Simon Property Group is a leader in the retail real estate industry, adapting to changing consumer trends and creating vibrant shopping destinations."
-    },
-    {
+    description: "Simon Property Group is a real estate investment trust (REIT) that focuses on owning and operating shopping malls, premium outlets, and other retail properties. The company's properties provide a diverse range of retail experiences, attracting shoppers and tenants alike. Simon Property Group is a leader in the retail real estate industry, adapting to changing consumer trends and creating vibrant shopping destinations.",
+    website: "https://www.simon.com/"
+  },
+  {
     id: 1005,
     ticker: "O",
     name: "Realty Income Corporation",
     price2023: 220.3,
     size: 95,
     type: "reits",
-    description: "Realty Income Corporation is a real estate investment trust (REIT) that specializes in single-tenant, net-leased properties. The company owns a diversified portfolio of retail, industrial, and office properties leased to tenants under long-term agreements. Realty Income's business model focuses on generating consistent rental income, making it attractive for income-oriented investors."
-    },
-    {
+    description: "Realty Income Corporation is a real estate investment trust (REIT) that specializes in single-tenant, net-leased properties. The company owns a diversified portfolio of retail, industrial, and office properties leased to tenants under long-term agreements. Realty Income's business model focuses on generating consistent rental income, making it attractive for income-oriented investors.",
+    website: "https://www.realtyincome.com/"
+  },
+  {
     id: 1006,
     ticker: "AVB",
     name: "AvalonBay Communities",
     price2023: 280.6,
     size: 75,
     type: "reits",
-    description: "AvalonBay Communities is a real estate investment trust (REIT) that develops, owns, and operates multifamily apartment communities. The company's properties are designed to provide residents with high-quality living spaces and amenities. AvalonBay Communities focuses on creating sustainable and vibrant communities, meeting the evolving needs of renters in various markets."
-    },
-    {
+    description: "AvalonBay Communities is a real estate investment trust (REIT) that develops, owns, and operates multifamily apartment communities. The company's properties are designed to provide residents with high-quality living spaces and amenities. AvalonBay Communities focuses on creating sustainable and vibrant communities, meeting the evolving needs of renters in various markets.",
+    website: "https://www.avaloncommunities.com/"
+  },
+  {
     id: 1007,
     ticker: "EQR",
     name: "Equity Residential",
     price2023: 210.9,
     size: 100,
     type: "reits",
-    description: "Equity Residential is a real estate investment trust (REIT) that specializes in owning and operating multifamily properties. The company's portfolio includes apartments in urban and high-density suburban areas, catering to a diverse range of residents. Equity Residential aims to provide quality housing options and exceptional living experiences for its tenants."
-    },
-    {
+    description: "Equity Residential is a real estate investment trust (REIT) that specializes in owning and operating multifamily properties. The company's portfolio includes apartments in urban and high-density suburban areas, catering to a diverse range of residents. Equity Residential aims to provide quality housing options and exceptional living experiences for its tenants.",
+    website: "https://www.equityapartments.com/"
+  },
+  {
     id: 1008,
     ticker: "DLR",
     name: "Digital Realty Trust",
     price2023: 260.1,
     size: 85,
     type: "reits",
-    description: "Digital Realty Trust is a real estate investment trust (REIT) that focuses on data center properties and colocation services. The company owns and operates data centers globally, providing secure and reliable infrastructure for storing, processing, and distributing digital information. Digital Realty Trust serves the needs of businesses in sectors such as technology, finance, and healthcare."
-    },
-    {
+    description: "Digital Realty Trust is a real estate investment trust (REIT) that focuses on data center properties and colocation services. The company owns and operates data centers globally, providing secure and reliable infrastructure for storing, processing, and distributing digital information. Digital Realty Trust serves the needs of businesses in sectors such as technology, finance, and healthcare.",
+    website: "https://www.digitalrealty.com/"
+  },
+  {
     id: 1009,
     ticker: "ARE",
     name: "Alexandria Real Estate Equities",
     price2023: 240.3,
     size: 120,
     type: "reits",
-    description: "Alexandria Real Estate Equities is a real estate investment trust (REIT) that specializes in life science and technology campuses. The company owns and operates properties that are tailored to the needs of innovative companies in the biotechnology, pharmaceutical, and technology sectors. Alexandria Real Estate Equities supports the growth of life science and technology ecosystems by providing state-of-the-art facilities and fostering collaboration."
-    }
+    description: "Alexandria Real Estate Equities is a real estate investment trust (REIT) that specializes in life science and technology campuses. The company owns and operates properties that are tailored to the needs of innovative companies in the biotechnology, pharmaceutical, and technology sectors. Alexandria Real Estate Equities supports the growth of life science and technology ecosystems by providing state-of-the-art facilities and fostering collaboration.",
+    website: "https://www.are.com/"
+  }
 ];
 
 module.exports = reitsData;
